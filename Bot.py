10c625b6268bc787a007f94485f4fc4d19435efc
Ian Brugal <IanPlaysGames@Gmail.com>
2019-08-19 00:32:47 -0400

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,3 +1,4 @@
+import time
 import random
 import requests
 import discord
@@ -7,10 +8,6 @@ tokenfile =  open("token.txt", "r")
 TOKEN = tokenfile.read()
 
 client = commands.Bot(command_prefix='~')
-
-@client.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
 @client.command(name='8ball')
 async def eightball(ctx):
@@ -31,5 +28,16 @@ async def gchange(ctx,*args):
         holder += (" "+i)
     game = discord.Game(holder)
     await client.change_presence(status=discord.Status.idle, activity=game)
+
+@client.command()
+async def oddsoutof(ctx,int:int,*args):
+    await ctx.send("you have 5 seconds to guess a number between 1 and "+str(int))
+    number = random.randrange(1,int)
+    holder = ""
+    for i in args:
+        holder += (" "+i)
+    time.sleep(5)
+    await ctx.send("If you guessed "+str(number)+holder)
+
 
 client.run(TOKEN)
